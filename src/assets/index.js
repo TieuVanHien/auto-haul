f036a135d5b635d3a923755cabba4705647ad296
Van Hien <hiench2001@gmail.com>
2022-11-18 10:02:46 -0700

    removed unused and added links to nav

--- src/assets/index.js
+++
@@ -1,1 +0,0 @@
-export const logo = require("./logo-svg.svg");
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,5 +1,77 @@
 import React from "react";
+import Logo from "./../assets/logo-svg.svg";
+import { Link } from "react-scroll";
 
-const Nav = () => {
-  return <div className="nav"></div>;
+export const Nav = () => {
+  return (
+    <section className="nav">
+      <div className="nav-container">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="links">
+          <Link
+            href="#home"
+            className="link"
+            to="home"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={700}
+            // onClick={() => {
+            //   setClose(false);
+            //   setOpen(false);
+            // }}
+          >
+            Home
+          </Link>
+          <Link
+            href="#about"
+            className="link"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={700}
+            // onClick={() => {
+            //   setClose(false);
+            //   setOpen(false);
+            // }}
+          >
+            About Us
+          </Link>
+          <Link
+            href="#service"
+            className="link"
+            to="service"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={700}
+            // onClick={() => {
+            //   setClose(false);
+            //   setOpen(false);
+            // }}
+          >
+            Service
+          </Link>
+          <Link
+            href="#Contact"
+            className="link"
+            to="Contact"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={700}
+            // onClick={() => {
+            //   setClose(false);
+            //   setOpen(false);
+            // }}
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
