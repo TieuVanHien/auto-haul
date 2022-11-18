0df3dd9775bd1a82bda7819e21ca5b2235e6794c
Van Hien <hiench2001@gmail.com>
2022-11-18 11:21:51 -0700

    install MUI package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { Nav } from "./components";
+import ReactDOM from "react-dom/client";
+import { Home } from "./section";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/section/home.js
+++ src/section/home.js
@@ -3,7 +3,17 @@ import React from "react";
 export const Home = () => {
   return (
     <section className="home">
-      <div className="container"></div>
+      <div className="container">
+        <div className="left-side">
+          <h1>Affordable Auto Hauler Service</h1>
+          <p>
+            We provide car shipping for cars, trucks, heavy duty work trucks and
+            exotic cars. Our team is experienced and professional car shipping
+            company
+          </p>
+        </div>
+        <div className="right-side"></div>
+      </div>
     </section>
   );
 };
