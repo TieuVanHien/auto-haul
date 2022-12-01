233e2aab72b12f2c79932295ec8e136b41b8031d
Van Hien <hiench2001@gmail.com>
2022-12-01 14:54:27 -0700

    updated in home section

--- src/section/home.js
+++ src/section/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "@mui/material/Button";
 import Rectangle from "./../images/rectangleHome.svg";
 import HomeImg from "./../images/nav-image.svg";
+import { Link } from "react-scroll";
 
 export const Home = () => {
   return (
@@ -16,7 +17,17 @@ export const Home = () => {
             company
           </p>
           <Button className="button" size="large" variant="contained">
-            Get Quote Now!
+            <Link
+              href="#form"
+              className="link"
+              to="form"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={200}
+            >
+              Get A Quote
+            </Link>
           </Button>
         </div>
         <div className="right-side">
